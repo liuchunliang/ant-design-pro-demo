596522e5014845579083262e69285f50b2b2800d
liuchunliang <liuchunliang@xincheng.com>
2020-11-16 09:24:55 +0800

    举报信息表空白列表页面

--- config/routes.js
+++ config/routes.js
@@ -46,6 +46,12 @@ export default [
             ],
           },
           {
+            path: '/tip-offs-list', // url中path部分，http://localhost:8000/todo状态跌幅到这个页面
+            name: 'list.tip-offs-list', // 名称，国际化菜单配置根据这个名称来配置，如果不配置将直菜单将直接显示这个名称
+            icon: 'table', // 菜单图标名称
+            component: './ListTipOffs', // 组件（页面）相对于src/pages的路径
+          },
+          {
             name: 'list.table-list',
             icon: 'table',
             path: '/list',
---
+++ src/pages/ListTipOffs/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { PageHeaderWrapper } from '@ant-design/pro-layout';
+
+const TipOffsPage = () => {
+  return (
+    <PageHeaderWrapper>
+      <div>空白面</div>
+    </PageHeaderWrapper>
+  );
+};
+
+export default TipOffsPage;
